4f15219928a0f5357aa4127d0fa0b1985275d6fd
davidGBbr <davidgomesbrigido@gmail.com>
2023-09-14 12:10:17 -0300

    Sidenav

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import DateRange from "./DateRange";
 import Meses from "./Meses";
 
 const Header = () => {
+  const [title, setTitle] = useState("Resumo");
+
   return (
     <header className="mb">
-      <div className="mb">
+      <div className="daterange mb">
         <DateRange />
+        <h1 className="box bg-3">{title}</h1>
       </div>
       <Meses />
     </header>
--- src/Components/Sidenav.tsx
+++ src/Components/Sidenav.tsx
@@ -1,7 +1,57 @@
 import React from "react";
+import fintech from "../assets/fintech.svg";
+import resumo from "../assets/icons/resumo.svg";
+import vendas from "../assets/icons/vendas.svg";
+import webhooks from "../assets/icons/webhooks.svg";
+import configuracoes from "../assets/icons/configuracoes.svg";
+import contato from "../assets/icons/contato.svg";
+import sair from "../assets/icons/sair.svg";
+import FintechSVG from "../assets/FintechSVG";
 
 const Sidenav = () => {
-  return <div>Sidenav</div>;
+  return (
+    <nav className="sidenav box bg-3">
+      <FintechSVG title="Fintech Logo" />
+      <ul>
+        <li>
+          <span>
+            <img src={resumo} alt="" />
+          </span>
+          <a>Resumo</a>
+        </li>
+        <li>
+          <span>
+            <img src={vendas} alt="" />
+          </span>
+          <a>Vendas</a>
+        </li>
+        <li>
+          <span>
+            <img src={webhooks} alt="" />
+          </span>
+          <a>Webhooks</a>
+        </li>
+        <li>
+          <span>
+            <img src={configuracoes} alt="" />
+          </span>
+          <a>Configurações</a>
+        </li>
+        <li>
+          <span>
+            <img src={contato} alt="" />
+          </span>
+          <a>Contato</a>
+        </li>
+        <li>
+          <span>
+            <img src={sair} alt="" />
+          </span>
+          <a>Sair</a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Sidenav;
