c9e366f2f705eefcc4e650f24675188d1969705d
davidGBbr <davidgomesbrigido@gmail.com>
2023-09-14 15:46:38 -0300

    Gráficos

---
+++ src/Components/GraficoVendas.tsx
@@ -0,0 +1,68 @@
+import {
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import { IVenda } from "../Context/DataContext";
+
+type VendaDia = {
+  data: string;
+  pago: number;
+  processando: number;
+  falha: number;
+};
+
+function transformData(data: IVenda[]): VendaDia[] {
+  const dias = data.reduce((acc: { [key: string]: VendaDia }, item) => {
+    const dia = item.data.split(" ")[0];
+    if (!acc[dia]) {
+      acc[dia] = {
+        data: dia,
+        pago: 0,
+        falha: 0,
+        processando: 0,
+      };
+    }
+    acc[dia][item.status] += item.preco;
+    return acc;
+  }, {});
+
+  return Object.values(dias).map((dia) => ({
+    ...dia,
+    data: dia.data.substring(5),
+  }));
+}
+
+const GraficoVendas = ({ data }: { data: IVenda[] }) => {
+  const transformedData = transformData(data);
+
+  return (
+    <ResponsiveContainer width="99%" height={400}>
+      <LineChart data={transformedData}>
+        <XAxis dataKey="data" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="pago" stroke="#A36AF9" strokeWidth={3} />
+        <Line
+          type="monotone"
+          dataKey="processando"
+          stroke="#FBCB21"
+          strokeWidth={3}
+        />
+        <Line
+          type="monotone"
+          dataKey="falha"
+          stroke="#000000"
+          strokeWidth={3}
+        />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default GraficoVendas;
--- src/Pages/Resumo.tsx
+++ src/Pages/Resumo.tsx
@@ -1,3 +1,4 @@
+import GraficoVendas from "../Components/GraficoVendas";
 import { useData } from "../Context/DataContext";
 
 const Resumo = () => {
@@ -35,7 +36,9 @@ const Resumo = () => {
           </span>
         </div>
       </div>
-      <div className="box mb">Gráficos</div>
+      <div className="box mb">
+        <GraficoVendas data={data} />
+      </div>
     </section>
   );
 };
